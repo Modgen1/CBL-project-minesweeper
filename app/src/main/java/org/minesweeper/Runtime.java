0b6b1da1fe1b6b35c894586f52073c8c92a173f8
Modgen1 <vanyappap@gmail.com>
2024-10-03 15:52:15 +0200

    Configured Gradle for minesweeper project

---
+++ app/src/main/java/org/minesweeper/Runtime.java
@@ -0,0 +1,11 @@
+package org.minesweeper;
+
+public class Runtime {
+    public void run() {
+        System.out.println("Hello world!");;
+    }
+
+    public static void main(String[] args) {
+        new Runtime().run();
+    }
+}
