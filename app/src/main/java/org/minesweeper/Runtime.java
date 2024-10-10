fa091d991214dbbfdebe8c03e392fc3c2e5652e2
Modgen1 <vanyappap@gmail.com>
2024-10-10 11:42:48 +0200

    Added support for switching between all screen variations in the runtime

--- app/src/main/java/org/minesweeper/Runtime.java
+++ app/src/main/java/org/minesweeper/Runtime.java
@@ -9,14 +9,14 @@ import javax.swing.*;
 public class Runtime {
     // runtime stores all screens after their first initialization so game can use same screens
     // instead of creating new ones every time
-    WelcomeScreen welcomeScreen;
-    GameScreen gameScreen;
+    WelcomeScreen welcomeScreen = null;
+    GameScreen gameScreen = null;
     WinScreen winScreen = null;
     LoseScreen loseScreen = null;
 
     // storing current screen makes program always know what is the current active screen
     // that is shown to the user
-    JFrame currentScreen;
+    JFrame currentScreen = null;
 
     // initializing settings that can be changed by the user so they are also available to every
     // window in the game through runtime
@@ -33,8 +33,12 @@ public class Runtime {
     /**
      * TODO write javadoc.
      */
-    public void start() {
-        welcomeScreen = new WelcomeScreen(this);
+    public void mainMenu() {
+        if (welcomeScreen == null) {
+            welcomeScreen = new WelcomeScreen(this);
+        } else {
+            currentScreen.setVisible(false);
+        }
         currentScreen = welcomeScreen;
         currentScreen.setVisible(true);
     }
@@ -49,7 +53,31 @@ public class Runtime {
         currentScreen.setVisible(true);
     }
 
+    /**
+     * TODO write javadoc.
+     */
+    public void winGame() {
+        currentScreen.setVisible(false);
+        if (winScreen == null) {
+            winScreen = new WinScreen(this);
+        }
+        currentScreen = winScreen;
+        currentScreen.setVisible(true);
+    }
+
+    /**
+     * TODO write javadoc.
+     */
+    public void loseGame() {
+        currentScreen.setVisible(false);
+        if (loseScreen == null) {
+            loseScreen = new LoseScreen(this);
+        }
+        currentScreen = loseScreen;
+        currentScreen.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        new Runtime().start();
+        new Runtime().mainMenu();
     }
 }
