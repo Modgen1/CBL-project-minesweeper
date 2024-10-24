a56d5e311f596ec90b2319e31706c6477d8181d4
Modgen1 <vanyappap@gmail.com>
2024-10-24 20:48:14 +0200

    Added README.md file with the full project description and also added more comments to the Runtime

--- app/src/main/java/org/minesweeper/Runtime.java
+++ app/src/main/java/org/minesweeper/Runtime.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import javax.swing.*;
 
 /**
+ * For more information about project itself, please read README.md.
  * Main package class that runs the program. Stores all created objects of screen classes,
  * the current screen, information regarding the user's screen resolution and all global variables
  * that can be accessed by other objects of the package.
@@ -26,9 +27,9 @@ public class Runtime {
 
     // initializing settings that can be changed by the user so they are also available to every
     // window in the game through runtime
-    int xMines = 12; // 8 is the minimal width of the field
-    int yMines = 12; // 8 is the minimal height of the field
-    int mineAmount = 18; // 8 is the minimal amount of mines on the smallest field
+    int xMines = 12; // 12 is the minimal width of the field
+    int yMines = 12; // 12 is the minimal height of the field
+    int mineAmount = 18; // 18 is the minimal amount of mines on the smallest field
 
     // getting user's screen resolution so it can be used by window objects to properly
     // scale game windows
@@ -40,11 +41,14 @@ public class Runtime {
      * Create or switch to a welcome screen at the launch of the program.
      */
     public void mainMenu() {
+        // if there is no created object of welcome screen, create new object
         if (welcomeScreen == null) {
             welcomeScreen = new WelcomeScreen(this);
+        // if there is, then we are switching from another screen, therefore turn off current screen
         } else {
             currentScreen.setVisible(false);
         }
+        // set welcome screen as current screen and show it
         currentScreen = welcomeScreen;
         currentScreen.setVisible(true);
     }
@@ -53,8 +57,11 @@ public class Runtime {
      * Switch from the current screen to the new game screen.
      */
     public void newGame() {
+        // set current screen as not visible
         currentScreen.setVisible(false);
+        // create new object of game screen class for this runtime
         gameScreen = new GameScreen(this);
+        // set game screen as current screen and show it
         currentScreen = gameScreen;
         currentScreen.setVisible(true);
     }
@@ -64,10 +71,13 @@ public class Runtime {
      * Switches from the game screen to the win screen.
      */
     public void winGame() {
+        // set current screen as not visible
         currentScreen.setVisible(false);
+        // if there is no created object of win screen, create new object
         if (winScreen == null) {
             winScreen = new WinScreen(this);
         }
+        // then set win screen as current screen and show it
         currentScreen = winScreen;
         currentScreen.setVisible(true);
     }
@@ -77,15 +87,19 @@ public class Runtime {
      * Switches from the game screen to the defeat screen.
      */
     public void loseGame() {
+        // firstly, wait three seconds for animation of lose
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
+        // then set current screen as not visible
         currentScreen.setVisible(false);
+        // if there is no created object of defeat screen, create new object
         if (loseScreen == null) {
             loseScreen = new LoseScreen(this);
         }
+        // then set defeat screen as current screen and show it
         currentScreen = loseScreen;
         currentScreen.setVisible(true);
     }
